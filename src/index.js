5323b2bf2611e61eb629acea5932c34bdc6965dc
EvgputWeb <EvgputWeb@yandex.ru>
2019-05-22 02:09:13 +0300

    ДЗ1: 1-ая итерация

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(a) {
+    return a;
 }
 
 /*
@@ -29,7 +30,8 @@ function returnFirstArgument() {
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults(a, b) {
+function sumWithDefaults(a, b = 100) {
+    return a+b;
 }
 
 /*
@@ -41,6 +43,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn();
 }
 
 /*
@@ -56,7 +59,14 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {
+function returnCounter(number = 0) {
+    let F = function() {
+        return ++F.num;
+    };
+
+    F.num = number;
+    
+    return F;
 }
 
 /*
@@ -69,6 +79,7 @@ function returnCounter(number) {
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
 function returnArgumentsArray() {
+    return [...arguments];
 }
 
 /*
@@ -87,6 +98,13 @@ function returnArgumentsArray() {
    console.log(newSum()) выведет 6
  */
 function bindFunction(fn) {
+    let args = Array.from(arguments).slice(1);
+
+    fn.bind(...args);
+
+    let F = fn.bind(null, ...args);
+ 
+    return F;    
 }
 
 export {
