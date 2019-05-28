305eab1c8a7c1a2342730b1d7d0f49ad852e5629
EvgputWeb <EvgputWeb@yandex.ru>
2019-05-29 01:50:16 +0300

    ДЗ #3:  1-ая итерация

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
+    if ((!Array.isArray(array)) || (array.length===0)) {
+        throw new Error('empty array');
+    }
+    if (!(typeof fn === 'function')) {
+        throw new Error('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (!fn(array[i])) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 /*
@@ -36,6 +49,19 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+    if ((!Array.isArray(array)) || (array.length===0)) {
+        throw new Error('empty array');
+    }
+    if (!(typeof fn === 'function')) {
+        throw new Error('fn is not a function');
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i])) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 /*
@@ -49,7 +75,22 @@ function isSomeTrue(array, fn) {
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {
+function returnBadArguments(fn, ...args) {
+    if (!(typeof fn === 'function')) {
+        throw new Error('fn is not a function');
+    }
+    let resultArr = [];
+
+    for (let i = 0; i < args.length; i++) {
+
+        try {
+            fn(args[i]);
+        } catch (e) {
+            resultArr.push(args[i]);
+        }
+    }
+
+    return resultArr;
 }
 
 /*
@@ -69,7 +110,25 @@ function returnBadArguments(fn) {
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+function calculator(number = 0) {
+    if (!(isFinite(number))) {
+        throw new Error('number is not a number');
+    }
+
+    return {
+        sum: (...args) => args.reduce((result, el) => result += el, number),
+        dif: (...args) => args.reduce((result, el) => result -= el, number),
+        div: (...args) => {
+            let zerosArr = args.filter( item => item == 0 );
+
+            if (zerosArr.length > 0) {
+                throw new Error('division by 0');
+            }
+
+            return args.reduce((result, el) => result /= el, number);
+        },
+        mul: (...args) => args.reduce((result, el) => result *= el, number),
+    };
 }
 
 /* При решении задач, пострайтесь использовать отладчик */
