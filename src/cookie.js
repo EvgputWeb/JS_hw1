d6ed8d72cf5900109fee34476b5cab29f4fbf71c
EvgputWeb <EvgputWeb@yandex.ru>
2019-06-15 01:43:34 +0300

    ДЗ#7:  1-ая итерация

--- src/cookie.js
+++ src/cookie.js
@@ -43,10 +43,99 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
+/*
+ Обработчик строки фильтра
+*/
 filterNameInput.addEventListener('keyup', function() {
     // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
 });
 
+/*
+ Обработчик кнопки "Добавить cookie"
+*/
 addButton.addEventListener('click', () => {
     // здесь можно обработать нажатие на кнопку "добавить cookie"
+    const cookieName = addNameInput.value.trim();
+    const cookieValue = addValueInput.value.trim();
+
+    if (cookieName === '') {
+        return;
+    }
+    // Добавляем в браузер
+    setCookie(cookieName, cookieValue);
+
+    // Добавляем в таблицу
+    const tr = listTable.querySelector(`tr[data-cookie-name="${cookieName}"]`);
+
+    if (tr) { // уже есть кука с этим именем
+        // Заменяем содержимое
+        tr.innerHTML = `<td>${cookieName}</td><td>${cookieValue}</td><td><button>Удалить</button></td>`;
+
+    } else { // нет куки с этим именем
+        const tr = document.createElement('tr');
+
+        tr.setAttribute('data-cookie-name', cookieName);
+        tr.innerHTML = `<td>${cookieName}</td><td>${cookieValue}</td><td><button>Удалить</button></td>`;
+        listTable.appendChild(tr);
+    }
+    // Очищаем поля ввода
+    addNameInput.value = '';
+    addValueInput.value = '';
 });
+
+/*
+ Обработчик кнопки "Удалить"
+*/
+listTable.addEventListener('click', (e) => {
+    if ((e.target.tagName ==='BUTTON')
+        && e.target.parentNode // родитель существует (это td)
+        && e.target.parentNode.parentNode // родитель родителя существует (это tr)
+        && e.target.parentNode.parentNode.hasAttribute('data-cookie-name')
+    ) {
+        const tr = e.target.parentNode.parentNode;
+        const cookieName = tr.dataset.cookieName;
+
+        // Удаляем куку из браузера
+        setCookie(cookieName, '', { expires: -1 });
+
+        // Удаляем строку из таблицы
+        tr.remove();
+    }
+});
+
+/*
+ Функция setCookie взята со странички:
+ https://learn.javascript.ru/cookie#funktsiya-setcookie-name-value-options
+ (слегка исправлена, чтобы eslint не ругался)
+*/
+function setCookie(name, value, options) {
+    options = options || {};
+    let expires = options.expires;
+
+    if (typeof expires == 'number' && expires) {
+        let d = new Date();
+
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    let updatedCookie = name + '=' + value;
+
+    for (let propName in options) {
+        if ({}.hasOwnProperty.call(options, propName)) {
+            updatedCookie += '; ' + propName;
+            let propValue = options[propName];
+
+            if (propValue !== true) {
+                updatedCookie += '=' + propValue;
+            }
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
